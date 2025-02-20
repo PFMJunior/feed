bc8efdd9d886794cb675a3b030d592fda652fe96
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-02-19 21:19:35 -0300

    Deixando o Avatar aceitando todos os parametros padroes de uma imagem sem dar erro, com o ...props

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,15 +3,13 @@ import { ImgHTMLAttributes } from "react"
 
 interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean;
-    avatar?  : string
 }
 
-export function Avatar({ hasBorder = true, avatar }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
     return(
         <img
             className={hasBorder ? styles.avatarWidthBorder : styles.avatar }
-            src={avatar ? avatar : "https://avatars.githubusercontent.com/u/55879493?v=4"}
-            alt=""
+            {...props}
         />
     );
 }
